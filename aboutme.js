b3dcaca06758d2d17a82296b697ce50e4b021b21
David Calderón <22837226+dcalderonlo@users.noreply.github.com>
2022-12-18 00:05:26 -0400

    Create file about.js
    Describe my data and the role I want to play in the future

---
+++ aboutme.js
@@ -0,0 +1,7 @@
+let name= "David Calderón";
+let profession= "Logística";
+let job= "Full Stack Developer";
+
+let together= `Hola, me llamo ${ name } y me dedico a ${ profession }. Estoy cursando este Máster porque me gustaría trabajar en ${ job }`;
+
+console.log (together);
